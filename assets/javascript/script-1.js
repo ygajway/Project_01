f871e61652784f94c7311fb7d3f54a1de8dac8ea
Brennan <brennancodes@gmail.com>
2019-10-23 21:18:44 -0500

    setting variables to script-1

--- assets/javascript/script-1.js
+++ assets/javascript/script-1.js
@@ -1,3 +1,9 @@
+var now = moment().format("L");
 var checkIn;
 var index = { date: now, mood: mScale, risk: rScale};
-checkIns.push(index);
+
+function sendCheck(){
+    checkIn.push(index);
+    localStorage.setItem("Check-Ins",JSON.stringify(checkIn));
+}
+var savedChecks = JSON.parse(localStorage.getItem("Check-Ins"));
