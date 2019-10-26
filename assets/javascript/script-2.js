57b42056b2ad6b44817bb1deb6a1bfe372b2f19a
Alex Griep <alexgriep@hotmail.com>
2019-10-26 13:43:32 -0500

    added back in updated branch to repo

--- assets/javascript/script-2.js
+++ assets/javascript/script-2.js
@@ -1,40 +1,35 @@
 var gifURL = "https://api.giphy.com/v1/gifs/random?tag=cats&api_key=dc6zaTOxFJmzC";
-var quoteURL = "http://api.forismatic.com/api/1.0/";
+var quoteURL = "https://quote-garden.herokuapp.com/quotes/random";
 var jokeURL = "https://icanhazdadjoke.com/";
 var proxy = "https://chriscastle.com/proxy/post.php?:proxy:";
 
 //pressing gif button to create a random cat gif
 $(".gif-button").on("click", function () {
+
     $.ajax({
         url: gifURL,
         method: "GET",
     }).then(function (response) {
         var newGif = $("<img>");
         newGif.attr("src", response.data.images.original.url);
-        $("body").append(newGif);
+        $(".gif-container").append(newGif);
     })
 })
 
 //pressing quote button to create a random quote
 $(".quote-button").on("click", function () {
     $.ajax({
-        method: "POST",
-        data: {
-            method:"getQuote",
-            format:"json",
-            lang:"en"
-        },
-        url: proxy + quoteURL,
-
+        method: "GET",
+        url: quoteURL,
     }).then(function (response) {
-        console.log(response);
-        //var newQuote = ("<div>");
-
+        var newQuote = $("<div>");
+        newQuote.text(response.quoteText);
+        $(".quote-container").append(newQuote);
     })
 })
 
 //pressing joke button to create a random dad joke
-$(".joke-button").on("click", function(){
+$(".joke-button").on("click", function () {
     $.ajax({
         accepts: {
             text: "application/json"
@@ -42,9 +37,14 @@ $(".joke-button").on("click", function(){
         dataType: "json",
         url: jokeURL,
         method: "GET",
-    }).then(function (response){
+    }).then(function (response) {
         var newJoke = $("<div>");
         newJoke.text(response.joke);
-        $("body").append(newJoke);
+        $(".joke-container").append(newJoke);
     })
+})
+
+//pressing music button to create music player
+$(".music-button").on("click", function () {
+
 })
