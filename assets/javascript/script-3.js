bf18217b4df5fa90c96436c5238a1409de4da41c
Brennan <brennancodes@gmail.com>
2019-11-01 16:01:32 -0500

    Fixing console errors on progress page when no localstorage existed. Changing language of sobriety date to clean date.

--- assets/javascript/script-3.js
+++ assets/javascript/script-3.js
@@ -9,52 +9,61 @@ var soberDays = (now.diff(soberDate,"days") - ((soberYears * 365) + (soberMonths
 var soberNumber = $(".soberNumber");
 var streakNumber = $(".streakNumber");
 var checkIns = JSON.parse(localStorage.getItem("checkIns"));
-var newEntryDate = checkIns[checkIns.length-1].date;
-var oldEntryDate = checkIns[checkIns.length-2].date;
-var newEntry = moment(newEntryDate,"MM/DD/YYYY");
-var oldEntry = moment(oldEntryDate,"MM/DD/YYYY");
-console.log(oldEntry);
-var testDate1 = moment("10/28/2019","MM/DD/YYYY")
-console.log(newEntry.diff(testDate1,"days"));
+if (!checkIns) {
+    streakNumber.text("Please check in again tomorrow to see your streak.");
+}
+else if (!checkIns) {
+    streakNumber.text("Please check in again tomorrow to see your streak.");
+}
+else {
+    var newEntryDate = checkIns[checkIns.length-1].date;
+    var oldEntryDate = checkIns[checkIns.length-2].date;
+    var newEntry = moment(newEntryDate,"MM/DD/YYYY");
+    var oldEntry = moment(oldEntryDate,"MM/DD/YYYY");
+    function calcStreak(){
+        var streakContainer = $("<h2>");
+        if (newEntry.diff(oldEntry) > 1) {
+            counter = 0;
+        }
+        else if (newEntry.diff(oldEntry) == 1) {
+            counter++;
+        }
+        streakNumber.append(streakContainer);
+        streakContainer.append(counter);
+    }
+    calcStreak();
+}
 
 var counter = 0;
 
 function calcSober(){
     var soberContainer = $("<h2>");
-    var years = now.diff(soberDate, 'years');
-    if (years != 0) {
-        soberDate.add(years, 'years');
+    if (!localSoberDate) {
+        soberNumber.text("Please click the settings button to establish the start of your clean streak.")
     }
+    else {
+        var years = now.diff(soberDate, 'years');
+        if (years != 0) {
+            soberDate.add(years, 'years');
+        }
 
-    var months = now.diff(soberDate, 'months');
-    soberDate.add(months, 'months');
-    
+        var months = now.diff(soberDate, 'months');
+        soberDate.add(months, 'months');
+        
 
-    var days = now.diff(soberDate, 'days');
-    if (years != 0) {
-        var soberLength = (years + ' years ' + months + ' months ' + days + ' days');
+        var days = now.diff(soberDate, 'days');
+        if (years != 0) {
+            var soberLength = (years + ' years ' + months + ' months ' + days + ' days');
+        }
+        else if (years == 0 && months == 0) {
+            var soberLength = (days + ' days');
+        }
+        else if (years == 0) {
+            var soberLength = (months + ' months ' + days + ' days');
+        }
+        soberContainer.append(soberLength);
+        soberNumber.append(soberContainer);
     }
-    else if (years == 0 && months == 0) {
-        var soberLength = (days + ' days');
-    }
-    else if (years == 0) {
-        var soberLength = (months + ' months ' + days + ' days');
-    }
-    soberContainer.append(soberLength);
-    soberNumber.append(soberContainer);
-
-}
-function calcStreak(){
-    var streakContainer = $("<h2>");
-    if (newEntry.diff(oldEntry) > 1) {
-        counter = 0;
-    }
-    else if (newEntry.diff(oldEntry) == 1) {
-        counter++;
-    }
-    streakNumber.append(streakContainer);
-    streakContainer.append(counter);
 }
 
 calcSober();
-calcStreak();
